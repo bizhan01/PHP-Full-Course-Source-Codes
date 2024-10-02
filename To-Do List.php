a1e22183e049ba6e3cd9ed39c4c79764efe50615
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-02 09:41:20 +0430

    New Commit

---
+++ To-Do List.php
@@ -0,0 +1,9 @@
+<?php
+$tasks = array("Task 1", "Task 2", "Task 3", "Task 4");
+echo "<h2>To-Do List</h2>";
+echo "<ul>";
+foreach ($tasks as $task) {
+    echo "<li>$task</li>";
+}
+echo "</ul>";
+?>
