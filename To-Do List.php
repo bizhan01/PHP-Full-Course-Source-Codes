fccfeb0fda3b4686b3bf924dd81e508108f9e172
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-02 09:43:26 +0430

    New Commit

--- To-Do List.php
+++ To-Do List.php
@@ -1,9 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-$tasks = array("Task 1", "Task 2", "Task 3", "Task 4");
-echo "<h2>To-Do List</h2>";
-echo "<ul>";
-foreach ($tasks as $task) {
-    echo "<li>$task</li>";
-}
-echo "</ul>";
+$imageUrl = 'https://via.placeholder.com/150';
+echo "<img src='$imageUrl' alt='Placeholder Image'>";
 ?>
+</body>
+</html>
