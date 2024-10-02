7f8bdf92bbb64ae816bbfbf7fd41306b53e878d7
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-02 09:33:46 +0430

    My Initial Commit

---
+++ Hello World.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP App</title>
+</head>
+<body>
+    <?php
+        $message = "Hello, World!";
+        echo "<h1>$message</h1>";
+    ?>
+</body>
+</html>
